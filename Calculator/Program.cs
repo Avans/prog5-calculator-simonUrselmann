30cdf7088464297f9e54d31ed248c44e14704e90
simonUrselmann <splursel@avans.nl>
2016-09-07 13:13:52 +0200

    done

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,8 +10,26 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Success!");
-            Console.ReadLine();
+            calc calcu = new calc();
+            
+            Console.WriteLine("Enter date of birth (dd-MM-yyyy");
+            String inputdate = Console.ReadLine();
+            DateTime result;
+            Console.WriteLine(calcu.ParseInput(inputdate, out result));
+            while (!calcu.ParseInput(inputdate, out result))
+            {
+                Console.WriteLine("Enter date of birth (dd-MM-yyyy");
+                inputdate = Console.ReadLine();
+            }
+            if (calcu.ParseInput(inputdate, out result))
+            {
+                Console.WriteLine(calcu.CalculateAge(result));
+                Console.WriteLine("Success!");
+                Console.ReadLine();
+
+            }
+                
+            
         }
     }
 }
---
+++ Calculator/calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class calc : IAgeCalculator
+    {
+
+        public bool ParseInput(string input, out DateTime result)
+        {
+              DateTime dt;
+              if(DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt))
+              {
+                  result = dt;
+                  return true;
+              }
+              result = dt;
+              return false;
+        }
+
+        
+
+        public int CalculateAge(DateTime dateOfBirth)
+        {
+            int age;
+            DateTime now = DateTime.Today;
+            age = now.Year - dateOfBirth.Year;
+            return age;
+
+        }
+    }
+}
